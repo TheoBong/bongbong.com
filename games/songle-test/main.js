f63996a8aac60e79a5408192191445878ed17e77
Theo <theo@bongbong.com>
2022-05-16 19:31:01 -0700

    Update main.js

--- games/songle-test/main.js
+++ games/songle-test/main.js
@@ -1,7 +1,7 @@
 var app = (function () {
   var artist = 'Joywave';
   var twitter = 'joywavez'
-  var startDate = "2022-05-15"
+  var startDate = "2022-06-15"
     const Cn = ue([
       "Carry On Wayward Son - Kansas",
         "What Does the Fox Say - Ylvis",
